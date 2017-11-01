14fbec1f854d1af1c9535de1824e6b20dc852c9b
ryota-murakami <dojce1048@gmail.com>
2017-11-01 22:31:40 +0900

    create apollo client

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
+// @flow
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import ApolloClient from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
+import { ApolloProvider, graphql } from 'react-apollo'
+
+const client = new ApolloClient({
+    link: new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cj9fb5po51o070117u0p3z4s0' }),
+    cache: new InMemoryCache().restore({})
+})
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
