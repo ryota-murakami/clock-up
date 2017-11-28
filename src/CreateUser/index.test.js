c32e6ce281f79adb47ebc3c28364910ee659b4fe
ryota-murakami <dojce1048@gmail.com>
2017-11-29 01:56:11 +0900

    breakdown testcase

--- src/CreateUser/index.test.js
+++ src/CreateUser/index.test.js
@@ -9,4 +9,18 @@ describe('<CreateUser />', () => {
     const wrapper = shallow(apolloWarapper(CreateUser))
     expect(toJson(wrapper)).toMatchSnapshot()
   })
+  describe('data.loading == true', () => {
+    it('Loadingが表示されること', () => {})
+  })
+  describe('data.loading == false', () => {
+    it('Loadingが表示されないこと', () => {})
+    describe('isFreshUser() == true', () => {
+      it('createUser()が起動すること', () => {})
+      it("<Redirect to={{ pathname: '/' }} /> がreturnされること", () => {})
+    })
+    describe('isFreshUser() == true', () => {
+      it('createUser()が起動しないこと', () => {})
+      it("<Redirect to={{ pathname: '/' }} /> がreturnされること", () => {})
+    })
+  })
 })
