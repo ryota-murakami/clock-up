c72e8e61d1ff73e51a0032e14b1d53d3b5684a25
ryota-murakami <dojce1048@gmail.com>
2017-12-02 23:59:31 +0900

    fix incorrect test

--- src/Login/index.test.js
+++ src/Login/index.test.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import toJson from 'enzyme-to-json'
+import Auth0Lock from 'auth0-lock'
 import { Login } from './index'
 
 describe('<Login />', () => {
   it('snapshotが一致すること', () => {
-    const wrapper = shallow(<Login clientId="aaaa" domain="eeee" />)
+    const clientId = 'aaaa'
+    const domain = 'eeee'
+    const lock = new Auth0Lock(clientId, domain)
+    const wrapper = shallow(<Login lock={lock} />)
+
     expect(toJson(wrapper)).toMatchSnapshot()
   })
 })
