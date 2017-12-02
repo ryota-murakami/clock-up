20c18edb378f24a525f6a66a72c0e3dad4d2cbaa
ryota-murakami <dojce1048@gmail.com>
2017-12-03 00:19:56 +0900

    add [lock]propsのみの受け渡しでErrorなくレンダリングされること

--- src/Login/index.test.js
+++ src/Login/index.test.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import toJson from 'enzyme-to-json'
 import Auth0Lock from 'auth0-lock'
 import { Login } from './index'
 
 describe('<Login />', () => {
-  it('snapshotが一致すること', () => {
+  function setup() {
     const clientId = 'aaaa'
     const domain = 'eeee'
     const lock = new Auth0Lock(clientId, domain)
     const wrapper = shallow(<Login lock={lock} />)
 
-    expect(toJson(wrapper)).toMatchSnapshot()
+    return wrapper
+  }
+
+  it('[lock]propsのみの受け渡しでErrorなくレンダリングされること', () => {
+    expect(() => setup()).not.toThrow()
   })
 })
