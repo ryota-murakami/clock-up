2079c3cfff76d934a46bcb4d3c2dc28fa3f35e98
ryota-murakami <dojce1048@gmail.com>
2017-12-05 22:24:49 +0900

    enhance login button story

--- stories/index.js
+++ stories/index.js
@@ -3,20 +3,15 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { linkTo } from '@storybook/addon-links'
-import { MemoryRouter } from 'react-router'
 import Auth0Lock from 'auth0-lock'
 
-import LoginButton from '../src/Login/LoginButton'
+import { LoginButton } from '../src/Login/LoginButton'
 import { Loading } from '../src/components/Loading'
 
 storiesOf('ログインページ', module).add('LoginButton', () => {
   const lock = new Auth0Lock('story', 'book')
 
-  return (
-    <MemoryRouter>
-      <LoginButton lock={lock} />
-    </MemoryRouter>
-  )
+  return <LoginButton lock={lock} />
 })
 
 storiesOf('汎用コンポーネント', module).add('Loading', () => {
