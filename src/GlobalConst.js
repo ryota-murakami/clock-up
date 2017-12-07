79dfbab4509d89c06cd0d467e9cac619db99b220
ryota-murakami <dojce1048@gmail.com>
2017-12-07 22:46:40 +0900

    add comment

--- src/GlobalConst.js
+++ src/GlobalConst.js
@@ -1,1 +1,8 @@
+/**
+ * use to apollo-client authentication token
+ * set by <Login />
+ * get by src/index.js <CreateUser />
+ * 
+ * @type {string}
+ */
 export const AUTH0_ID_TOKEN = 'auth0IdToken'
