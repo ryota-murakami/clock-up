bcf848482a58a6720ab0640723395587f08183aa
ryota-murakami <dojce1048@gmail.com>
2017-12-15 03:26:42 +0900

    add test

--- src/App/Control.test.js
+++ src/App/Control.test.js
@@ -9,4 +9,31 @@ describe('<Control />', () => {
       expect(() => shallow(<Control data={incorrectData} />)).toThrow()
     })
   })
+
+  describe('during clock-in', () => {
+    const data = {
+      user: { foo: 'bar', isDuringClockIn: true }
+    }
+    it('should be show ClockoutBtn', () => {
+      const wrapper = shallow(<Control data={data} />)
+
+      expect(wrapper.find('Apollo(Apollo(ClockoutBtn))').exists()).toBe(true)
+    })
+    it('should be show ClokinTime', () => {
+      const wrapper = shallow(<Control data={data} />)
+
+      expect(wrapper.find('ClockinTime').exists()).toBe(true)
+    })
+  })
+
+  describe('during clock-out', () => {
+    const data = {
+      user: { foo: 'bar', isDuringClockIn: false }
+    }
+    it('should be show ClockinBtn', () => {
+      const wrapper = shallow(<Control data={data} />)
+
+      expect(wrapper.find('Apollo(Apollo(ClockinBtn))').exists()).toBe(true)
+    })
+  })
 })
