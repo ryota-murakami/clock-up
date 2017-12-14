4db60d965d3f418dd3f9f8fcf0c9f4065bf11bdc
ryota-murakami <dojce1048@gmail.com>
2017-12-15 03:27:45 +0900

    tweek

--- src/App/Control.test.js
+++ src/App/Control.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import Control from './Control'
+import { Control } from './Control'
 
 describe('<Control />', () => {
   describe('incorrect data', () => {
