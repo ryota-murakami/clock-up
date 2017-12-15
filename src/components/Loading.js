27c7671175523aa70e6fa5799fd227941a7edb58
ryota-murakami <dojce1048@gmail.com>
2017-12-15 11:11:17 +0900

    loading style

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,12 +1,27 @@
 // @flow
 import React from 'react'
+import styled from 'styled-components'
+import { BounceLoader } from 'react-spinners'
 
 type Props = {}
 
 export class Loading extends React.Component<Props> {
   render() {
-    return <div>Loading</div>
+    return (
+      <Container>
+        <BounceLoader color={'#36D7B7'} size={100} />
+      </Container>
+    )
   }
 }
+
+const Container = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`
 
 export default Loading
--- src/components/Loading.test.js
+++ src/components/Loading.test.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
 import { Loading } from './Loading'
 
 describe('<Loading />', () => {
-  it('正しくレンダリングされること', () => {
+  const setup = function() {
     const wrapper = shallow(<Loading />)
-    expect(wrapper.html()).toEqual('<div>Loading</div>')
+    return wrapper
+  }
+
+  it('should be render', () => {
+    const wrapper = setup()
+    expect(wrapper.exists()).toBe(true)
+  })
+
+  it('should be match snapshot', () => {
+    const wrapper = setup()
+
+    expect(toJson(wrapper)).toMatchSnapshot()
   })
 })
