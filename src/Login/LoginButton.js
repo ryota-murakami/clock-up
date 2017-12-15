6ce951481f01f3c0a774c1a5ea7060f3c0c3c04c
ryota-murakami <dojce1048@gmail.com>
2017-12-15 10:49:22 +0900

    loginbutton styled

--- src/Login/LoginButton.js
+++ src/Login/LoginButton.js
@@ -1,5 +1,6 @@
 // @flow
 import React, { Component } from 'react'
+import styled from 'styled-components'
 import Auth0Lock from 'auth0-lock'
 import { withRouter } from 'react-router-dom'
 import { AUTH0_ID_TOKEN } from '../GlobalConst'
@@ -45,13 +46,23 @@ export class LoginButton extends Component<Props> {
 
   render() {
     return (
-      <div>
-        <span onClick={this.showLogin} data-test="login-btn">
-          Google Login
-        </span>
-      </div>
+      <Button onClick={this.showLogin} data-test="login-btn">
+        Login
+      </Button>
     )
   }
 }
+
+const Button = styled.div`
+  width: 70px;
+  color: #66bb6a;
+  font-size: 1.1em;
+  line-height: 28px;
+  padding: 0.25em 1em;
+  border: 2px solid #66bb6a;
+  border-radius: 3px;
+  text-align: center;
+  cursor: pointer;
+`
 
 export default withRouter(LoginButton)
