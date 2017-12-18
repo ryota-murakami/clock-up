9247cd21130177fdbdffe6d16b3ef149c995b9e2
ryota-murakami <dojce1048@gmail.com>
2017-12-18 20:18:39 +0900

    Header snapshot

---
+++ src/components/Header.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
+import { Header } from './Header'
+
+describe('<Header />', () => {
+  it('toMatchSnapshot', () => {
+    const wrapper = shallow(<Header />)
+
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+})
