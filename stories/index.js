6a7c442947fe7dd315bd523fce2c83aa8c8a9572
ryota-murakami <dojce1048@gmail.com>
2017-12-18 21:06:01 +0900

    align storybook

--- stories/index.js
+++ stories/index.js
@@ -13,17 +13,15 @@ import { ClockinButton } from '../src/App/Control/ClockinButton'
 import { ClockoutButton } from '../src/App/Control/ClockoutButton'
 import ClockinTime from '../src/App/Control/ClockinTime'
 
-storiesOf('ログインページ', module).add('LoginButton', () => {
-  const lock = new Auth0Lock('story', 'book')
+storiesOf('コンポーネント一覧', module)
+  .add('LoginButton', () => {
+    const lock = new Auth0Lock('story', 'book')
 
-  return <LoginButton lock={lock} />
-})
-
-storiesOf('汎用コンポーネント', module).add('Loading', () => {
-  return <Loading />
-})
-
-storiesOf('メインページ', module)
+    return <LoginButton lock={lock} />
+  })
+  .add('Loading', () => {
+    return <Loading />
+  })
   .add('CurrentDateTime', () => {
     return <CurrentDateTime />
   })
