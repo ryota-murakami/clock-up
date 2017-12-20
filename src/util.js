df1df9500d68c138e998159101e8dd6bb806c4e7
ryota-murakami <dojce1048@gmail.com>
2017-12-20 22:42:51 +0900

    remove deadcode

--- src/util.js
+++ src/util.js
@@ -46,10 +46,8 @@ export function calcTimeDiff(now: Date, past: Date): string {
   const hh = Math.floor(msec / 1000 / 60 / 60)
   msec -= hh * 1000 * 60 * 60
   const mm = Math.floor(msec / 1000 / 60)
-  msec -= mm * 1000 * 60
-  const ss = Math.floor(msec / 1000)
 
-  return pad2(hh) + 'h' + pad2(mm) + 'm' // + pad2(ss) + 's'
+  return pad2(hh) + 'h' + pad2(mm) + 'm'
 }
 
 /**
