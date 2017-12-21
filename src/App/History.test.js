8af79949f31b55cc4a7d1738fe9a1ba06853e36c
ryota-murakami <dojce1048@gmail.com>
2017-12-21 11:47:05 +0900

    add history first time data test

--- src/App/History.test.js
+++ src/App/History.test.js
@@ -33,7 +33,49 @@ describe('<History />', () => {
           .first()
           .dive()
           .text()
-      ).toEqual('N/A')
+      ).toBe('N/A')
+    })
+
+    it('should be match snapshot', () => {
+      const wrapper = setup()
+
+      expect(toJson(wrapper)).toMatchSnapshot()
+    })
+  })
+  describe('pass first time clock in data', () => {
+    const clocks = [
+      {
+        id: 'cjbfuqo1j3n5p0146x0pjsj7o',
+        clockIn: '2017-12-21T02:12:28.707Z',
+        clockOut: null,
+        createdAt: '2017-12-21T02:12:28.000Z',
+        updatedAt: '2017-12-21T02:12:28.000Z',
+        __typename: 'Clock'
+      }
+    ]
+    function setup() {
+      const wrapper = shallow(<History clocks={clocks} />)
+      return wrapper
+    }
+
+    it('should be render with N/A', () => {
+      const wrapper = setup()
+
+      expect(wrapper.find('Table__Td').exists()).toBe(true)
+      expect(
+        wrapper
+          .find('Table__Td')
+          .first()
+          .dive()
+          .exists()
+      ).toBe(true)
+      expect(
+        wrapper
+          .find('Table__Td')
+          .first()
+          .dive()
+          .text()
+      ).toBe('N/A')
     })
 
     it('should be match snapshot', () => {
