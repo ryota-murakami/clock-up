26e4eea56999b23c9978f061495962a18b09fa0b
ryota-murakami <dojce1048@gmail.com>
2017-12-21 12:09:08 +0900

    fix

--- src/common/testUtil.js
+++ src/common/testUtil.js
@@ -4,34 +4,3 @@ import React from 'react'
 export function sel(id: string): string {
   return `[data-test="${id}"]`
 }
-
-const stripWhitespace = (str: string) =>
-  str
-    .trim()
-    .replace(/([;\{\}])/g, '$1  ')
-    .replace(/\s+/g, ' ')
-
-const stripComments = (str: string) => str.replace(/\/\*.*?\*\/\n?/g, '')
-
-export const expectCSSMatches = (
-  _expectation: string,
-  opts: { ignoreWhitespace: boolean } = { ignoreWhitespace: true }
-) => {
-  // NOTE: This should normalise both CSS strings to make irrelevant mismatches less likely
-  const expectation = _expectation.replace(/ {/g, '{').replace(/:\s+;/g, ':;')
-
-  const css = Array.from(document.querySelectorAll('style'))
-    .map(tag => tag.innerHTML)
-    .join('\n')
-    .replace(/ {/g, '{')
-    .replace(/:\s+;/g, ':;')
-
-  if (opts.ignoreWhitespace) {
-    const stripped = stripWhitespace(stripComments(css))
-    expect(stripped).toEqual(stripWhitespace(expectation))
-    return stripped
-  } else {
-    expect(css).toEqual(expectation)
-    return css
-  }
-}
