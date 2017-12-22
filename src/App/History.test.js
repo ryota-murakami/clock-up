fc873824c74240860961dfe27a44db6bedecd221
ryota-murakami <dojce1048@gmail.com>
2017-12-22 20:49:48 +0900

    add history complete test

--- src/App/History.test.js
+++ src/App/History.test.js
@@ -84,4 +84,44 @@ describe('<History />', () => {
       expect(toJson(wrapper)).toMatchSnapshot()
     })
   })
+  describe('pass complete data', () => {
+    const clocks = [
+      {
+        id: 'cjbhtpf8psjmv0146zkmd9zrh',
+        clockIn: '2017-12-22T11:19:02.776Z',
+        clockOut: null,
+        createdAt: '2017-12-22T11:19:03.000Z',
+        updatedAt: '2017-12-22T11:19:03.000Z',
+        __typename: 'Clock'
+      },
+      {
+        id: 'cjbfuqo1j3n5p0146x0pjsj7o',
+        clockIn: '2017-12-21T02:12:28.707Z',
+        clockOut: '2017-12-21T16:54:21.094Z',
+        createdAt: '2017-12-21T02:12:28.000Z',
+        updatedAt: '2017-12-21T16:54:21.000Z',
+        __typename: 'Clock'
+      }
+    ]
+    function setup() {
+      const wrapper = shallow(<History clocks={clocks} />)
+      return wrapper
+    }
+
+    it('should be render TotalTime', () => {
+      const wrapper = setup()
+
+      expect(wrapper.find('tr').exists()).toBe(true)
+      expect(wrapper.html().includes('2017/12/21')).toBe(true) // "date" column
+      expect(wrapper.html().includes('14h41m')).toBe(true) // "total" column
+      expect(wrapper.html().includes('11:12')).toBe(true) // "in" column
+      expect(wrapper.html().includes('01:54')).toBe(true) // "out" column
+    })
+
+    it('should be match snapshot', () => {
+      const wrapper = setup()
+
+      expect(toJson(wrapper)).toMatchSnapshot()
+    })
+  })
 })
