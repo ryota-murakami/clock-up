fc1327cc2af380317b2f06bd485489344f4e732c
ryota-murakami <dojce1048@gmail.com>
2017-12-25 18:58:51 +0900

    add state tree comment

--- src/types/ReduxState.js
+++ src/types/ReduxState.js
@@ -2,6 +2,31 @@
 import type { CurrentTime } from './CurrentTime'
 
 /**
- * TODO object expression
+ * ↓↓ is entire Redux State tree.
+ * app is whole of end-developer land.(it means code of src/**)
+ * router is react-router-redux liblary land.
+ *
+ * {
+ *  router: {
+ *    location: {
+ *      pathname: '/',
+ *      search: '',
+ *      hash: '',
+ *      key: '2chitk'
+ *    }
+ *  },
+ *  app: {
+ *    currentTime: {
+ *      dateObject: '2017-12-25T09:54:44.207Z',
+ *      year: '2017',
+ *      month: 'December',
+ *      days: 'Monday',
+ *      date: '25',
+ *      hour: '18',
+ *      minutes: '54',
+ *      seconds: '44'
+ *    }
+ *  }
+ *}
  */
 export type ReduxState = { currentTime: CurrentTime }
