508a00401af287792315f714bd49c488286ae97d
ryota-murakami <dojce1048@gmail.com>
2017-12-25 17:18:52 +0900

    add CurrentTime types

--- src/App/actions.js
+++ src/App/actions.js
@@ -1,9 +1,10 @@
 import { parseTime } from '../common/util'
+import type { CurrentTime } from '../types/CurrentTime'
 
 export const SYNC_DATE = 'SYNC_DATE'
 
 export function syncDate() {
-  const time = parseTime(new Date())
+  const time: CurrentTime = parseTime(new Date())
   return {
     type: SYNC_DATE,
     ...time
