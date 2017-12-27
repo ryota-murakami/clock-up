89d296168263ffbd992bd7dd3d5145294b0bcba6
ryota-murakami <dojce1048@gmail.com>
2017-12-27 23:12:53 +0900

    todo

--- src/types/ReduxAction.js
+++ src/types/ReduxAction.js
@@ -4,6 +4,6 @@ import type { CurrentTime } from './CurrentTime'
 
 export type SyncDateAction = {| type: 'SYNC_DATE', currentTime: CurrentTime |}
 
-type FooAction = {| type: 'FOO', foo: number |}
+type FooAction = {| type: 'FOO', foo: number |} // TODO dummy
 
 export type ReduxAction = SyncDateAction | FooAction
