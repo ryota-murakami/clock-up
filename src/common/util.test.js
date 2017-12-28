b38f440123700c5831752a9b38fa752596733cf1
ryota-murakami <dojce1048@gmail.com>
2017-12-28 20:53:19 +0900

    add parseTime test

---
+++ src/common/util.test.js
@@ -0,0 +1,20 @@
+import { parseTime } from './util'
+
+describe('parseTime', () => {
+  it('should be return CurrentDate when passed Date', () => {
+    const date = new Date('2017/1/1 12:00:00')
+    const expected = {
+      dateObject: date,
+      year: '2017',
+      month: 'January',
+      days: 'Sunday',
+      date: '01',
+      hour: '12',
+      minutes: '00',
+      seconds: '0'
+    }
+    const actual = parseTime(date)
+
+    expect(actual).toEqual(expected)
+  })
+})
