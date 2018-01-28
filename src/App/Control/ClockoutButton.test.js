698e1582400bafa61e04070802c712eeb5a7d873
ryota-murakami <dojce1048@gmail.com>
2018-01-28 14:15:44 +0900

    fix <ClockoutButton /> test

--- src/App/Control/ClockoutButton.test.js
+++ src/App/Control/ClockoutButton.test.js
@@ -4,8 +4,20 @@ import { ClockoutButton } from './ClockoutButton'
 import { sel } from '../../common/testUtil'
 
 describe('<ClockoutButton />', () => {
+  const data = {
+    user: {
+      id: 'id',
+      clocks: [
+        {
+          id: 'id'
+        }
+      ]
+    },
+    loading: false
+  }
+
   it('should be render', () => {
-    const wrapper = shallow(<ClockoutButton />)
+    const wrapper = shallow(<ClockoutButton data={data} />)
 
     expect(wrapper.exists()).toBe(true)
   })
@@ -13,7 +25,7 @@ describe('<ClockoutButton />', () => {
   it('should be invoke recordClockoutTimeToGraphcool() when clicked', () => {
     const mockFunc = jest.fn()
     ClockoutButton.prototype.gqlLogic = mockFunc
-    const wrapper = shallow(<ClockoutButton />)
+    const wrapper = shallow(<ClockoutButton data={data} />)
 
     wrapper.find(sel('clock-out-btn')).simulate('click')
 
