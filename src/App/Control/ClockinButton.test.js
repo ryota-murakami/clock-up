d3a38f02af598c02236a021a6437e55f76d48026
ryota-murakami <dojce1048@gmail.com>
2018-02-04 23:18:49 +0900

    fix

--- src/App/Control/ClockinButton.test.js
+++ src/App/Control/ClockinButton.test.js
@@ -4,8 +4,12 @@ import { ClockinButton } from './ClockinButton'
 import { sel } from '../../common/testUtil'
 
 describe('<ClockinButton />', () => {
+  const data = {
+    loading: false
+  }
+
   it('should be render', () => {
-    const wrapper = shallow(<ClockinButton />)
+    const wrapper = shallow(<ClockinButton data={data} />)
 
     expect(wrapper.exists()).toBe(true)
   })
@@ -13,7 +17,7 @@ describe('<ClockinButton />', () => {
   it('should be invoke recordClockinTimeToGraphcool() when clicked', () => {
     const mockFunc = jest.fn()
     ClockinButton.prototype.gqlLogic = mockFunc
-    const wrapper = shallow(<ClockinButton />)
+    const wrapper = shallow(<ClockinButton data={data} />)
 
     wrapper.find(sel('clock-in-btn')).simulate('click')
 
