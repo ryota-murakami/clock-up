a0587db10d9f3476c868349f25a3e6328569944d
ryota-murakami <dojce1048@gmail.com>
2018-05-23 02:19:16 +0900

    refactor

--- src/pages/App/CurrentDateTime.js
+++ src/pages/App/CurrentDateTime.js
@@ -1,34 +1,8 @@
 // @flow
 import React, { Component } from 'react'
-import styled from 'styled-components'
-import { theme } from '../../const'
+import { Container, Day, Time } from './CurrentDateTime.style'
 import { connect } from 'react-redux'
 import type { CurrentTime } from '../../types/ReduxState'
-
-const Container = styled.div`
-  height: 25%;
-  border: 1px solid ${theme.borderColor};
-  margin: 20px;
-  padding: 60px;
-  border-radius: 5px;
-`
-
-const Day = styled.div`
-  height: 30%;
-  color: ${theme.textColor};
-  font-size: 1.5em;
-  text-align: center;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-`
-
-const Time = styled.div`
-  height: 70%;
-  color: ${theme.textColor};
-  font-size: 7em;
-  text-align: center;
-`
 
 type StateProps = {|
   currentTime: CurrentTime
---
+++ src/pages/App/CurrentDateTime.style.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+import { theme } from '../../const'
+
+export const Container = styled.div`
+  height: 25%;
+  border: 1px solid ${theme.borderColor};
+  margin: 20px;
+  padding: 60px;
+  border-radius: 5px;
+`
+
+export const Day = styled.div`
+  height: 30%;
+  color: ${theme.textColor};
+  font-size: 1.5em;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`
+
+export const Time = styled.div`
+  height: 70%;
+  color: ${theme.textColor};
+  font-size: 7em;
+  text-align: center;
+`
