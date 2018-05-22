f931d4d877a77c668346c6951ee1897391168880
ryota-murakami <dojce1048@gmail.com>
2018-05-23 01:27:53 +0900

    up

--- src/pages/App/History/InTime.js
+++ src/pages/App/History/InTime.js
@@ -1,13 +1,22 @@
 // @flow
-import React from 'react'
+import React, { Component } from 'react'
 import { Td } from '../../../elements/Table'
 
 type Props = {|
   date: string
 |}
 
-export const InTime = ({ date }: Props) => {
-  return <Td onClick={() => alert('click')}>{date}</Td>
+export class InTime extends Component<Props> {
+  handleClick() {
+    // TODO show nunber input & timepicker
+    return () => alert('click')
+    // TODO when decided, throw gql mutation
+  }
+
+  render() {
+    const { date } = this.props
+    return <Td onClick={this.handleClick()}>{date}</Td>
+  }
 }
 
 export default InTime
--- src/pages/App/History/OutTime.js
+++ src/pages/App/History/OutTime.js
@@ -1,13 +1,22 @@
 // @flow
-import React from 'react'
+import React, { Component } from 'react'
 import { Td } from '../../../elements/Table'
 
 type Props = {|
   date: string
 |}
 
-export const OutTime = ({ date }: Props) => {
-  return <Td onClick={() => alert('OutTime')}>{date}</Td>
+export class OutTime extends Component<Props> {
+  handleClick() {
+    // TODO show nunber input & timepicker
+    return () => alert('OutTime')
+    // TODO when decided, throw gql mutation
+  }
+
+  render() {
+    const { date } = this.props
+    return <Td onClick={this.handleClick()}>{date}</Td>
+  }
 }
 
 export default OutTime
