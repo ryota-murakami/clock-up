24f3f50c9bd1d07468b2939082b89b3aaccab3e2
ryota-murakami <dojce1048@gmail.com>
2018-06-20 21:34:06 +0900

    up

--- src/pages/App/Control/index.test.js
+++ src/pages/App/Control/index.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import { Control } from './index'
-import { sel } from "../../../testutil";
+import { sel } from '../../../testutil'
 
 describe('<Control />', () => {
   describe('incorrect data', () => {
