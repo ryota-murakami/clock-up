abf2e0d46c00a19b5bd5129c10cc093c2a769f4a
ryota-murakami <dojce1048@gmail.com>
2018-06-21 16:44:10 +0900

    move

--- src/pages/App/Control/ClockinTime.js
+++ src/pages/App/Control/ClockinTime.js
@@ -1,25 +1,10 @@
 // @flow
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import styled from 'styled-components'
-import { theme } from '../../../const'
 import { calcTotalTime } from '../../../function'
+import { Container } from './ClockinTime.style'
 import type { CurrentTime } from '../../../dataType'
 import type { ReduxState } from '../../../reducer'
-
-const Container = styled.div`
-  flex-basics: max-content;
-  flex-grow: 3;
-  color: ${theme.textColor};
-  font-size: 1.1em;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-around;
-  border: 1px solid ${theme.borderColor};
-  border-radius: 5px;
-  margin-right: 10px;
-`
 
 type User = {
   id: string,
---
+++ src/pages/App/Control/ClockinTime.style.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+import { theme } from '../../../const'
+
+export const Container = styled.div`
+  flex-basics: max-content;
+  flex-grow: 3;
+  color: ${theme.textColor};
+  font-size: 1.1em;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  border: 1px solid ${theme.borderColor};
+  border-radius: 5px;
+  margin-right: 10px;
+`
