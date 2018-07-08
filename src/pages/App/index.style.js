89f30ed8b58fc7250869ccfc59be2931afc5ef57
ryota-murakami <dojce1048@gmail.com>
2018-07-08 16:10:55 +0900

    fix width

--- src/pages/App/index.style.js
+++ src/pages/App/index.style.js
@@ -4,6 +4,8 @@ import { theme } from '../../theme'
 export const Container = styled.main`
   @media (min-width: 768px) {
     width: 100%;
+    max-width: 1180px;
+    margin: 0 auto;
     height: 100%;
     display: grid;
     grid-template-columns: repeat(3, 1fr);
