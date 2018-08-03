dfdfebe71b4465540f43a54025087bc7fd016dba
ryota-murakami <dojce1048@gmail.com>
2018-08-03 18:34:08 +0900

    fix config

--- config-overrides.js
+++ config-overrides.js
@@ -2,14 +2,6 @@
 const rewireStyledComponents = require('react-app-rewire-styled-components')
 
 module.exports = function override(config, env) {
-  // remove eslint-loader from webpack
-  if (
-    config.module.rules[0] &&
-    config.module.rules[0].use[0].loader.includes('eslint-loader')
-  ) {
-    config.module.rules.splice(0, 1)
-  }
-
   config = rewireStyledComponents(config, env, {
     ssr: false,
     displayName: true
