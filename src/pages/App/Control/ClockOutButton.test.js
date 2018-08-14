86cf80af525ef65795caf9c52153d2ae0829f75e
ryota-murakami <dojce1048@gmail.com>
2018-08-15 01:41:15 +0900

    fix

--- src/pages/App/Control/ClockOutButton.test.js
+++ src/pages/App/Control/ClockOutButton.test.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import { ClockOutButton } from './ClockOutButton'
-import { sel } from '../../../setupTests'
 
 describe('<ClockoutButton />', () => {
   const data = {
