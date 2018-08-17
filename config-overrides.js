be5943ac69e347f6a6403488a3ea30a40af2aa7b
ryota-murakami <dojce1048@gmail.com>
2018-08-18 01:30:41 +0900

    enable rewire

--- config-overrides.js
+++ config-overrides.js
@@ -2,10 +2,7 @@
 const rewireStyledComponents = require('react-app-rewire-styled-components')
 
 module.exports = function override(config, env) {
-  config = rewireStyledComponents(config, env, {
-    ssr: false,
-    displayName: true
-  })
+  config = rewireStyledComponents(config, env)
 
   return config
 }
